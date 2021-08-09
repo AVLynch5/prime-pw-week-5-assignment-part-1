3583b0d045e33788ba9a82290942b2be4ff77726
TJ Lynch <anthonyvlynch5@gmail.com>
2021-08-09 15:28:58 -0500

    Wk5-pt1 updated object-practice.js with required steps and function attribute

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,32 +10,41 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Anthony',
+  lastName: 'Lynch',
+  hasSiblings: false,
+  shoeCount: 6,
+  favThreeFoods: ['spaghetti', 'kimbap', 'banana']
 };
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
-
-
+let fullName = me.firstName+' '+me.lastName;
+console.log(fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
-
+console.log('My first favorite food is:', me.favThreeFoods[0]);
+console.log('My last favorite food is:', me.favThreeFoods[2]);
 
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
-
+console.log('My current shoe count is:', me.shoeCount);
+console.log('I got a new pair of shoes!');
+me.shoeCount += 2;
+console.log('My new shoe count is:', me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +52,22 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = 'blue';
+console.log(me);
+
+//stretchgoal -  adding a function to an object to call itself
+function getfullName (first, last) {
+  return first+' '+last;
+}//end getfullName, string contex, not coupled
+console.log(getfullName(me.firstName, me.lastName));
+
+function getfullnamePerson (me) {
+  return me.firstName+' '+me.lastName;
+}//end getfullnamePerson, coupled to me object
+console.log(getfullnamePerson(me));
+
+me.getPersonName = function getPersonName () {
+  return this.firstName+' '+this.lastName;
+}//end getPersonName, adds function to object as attribute, refers to self
+console.log(me.getPersonName());
+console.log(me);
